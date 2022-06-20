5ea22a8e1570ce674156c06bd2c9c807747cde60
Harshid Khunt <harshidkhunt09@gmail.com>
2022-06-20 11:08:02 +0530

    Initial Commit

---
+++ my-blog-frontend/src/App.js
@@ -0,0 +1,27 @@
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticleListPage from './pages/ArticleListPage';
+import ArticlePage from './pages/ArticlePage';
+import NotFoundPage from './pages/NotFoundPage';
+import NavBar from './components/NavBar';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <div id='page-body'>
+        <Routes>
+          <Route path='/' element={<HomePage />} exact />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/articles-list' element={<ArticleListPage />} />
+          <Route path='/article/:name' element={<ArticlePage />} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
