c03ce8b996a944038a774fe66d7a69140e8412d7
Harshid Khunt <harshidkhunt09@gmail.com>
2022-06-25 18:47:43 +0530

    Implemented Search Functionality

--- my-blog-frontend/src/pages/ArticleListPage.js
+++ my-blog-frontend/src/pages/ArticleListPage.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ArticleList from '../components/ArticleList';
 import articleContent from './article-content';
 
 const ArticleListPage = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const FilteredArticleList = articleContent.filter((article) => {
+    return article.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
   return (
     <div id='articles-body'>
       <h1>Articles</h1>
-      <input type='text' />
-      <ArticleList articles={articleContent} />
+      <input
+        type='text'
+        placeholder='Search Articles'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <br />
+      <button>Ascending</button>
+      <button>Descending</button>
+      <ArticleList
+        articles={searchTerm < 1 ? articleContent : FilteredArticleList}
+      />
     </div>
   );
 };
