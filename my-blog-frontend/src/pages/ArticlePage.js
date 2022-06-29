41c7d29ee75290e85532c1722f4d80d884d041ad
Harshid Khunt <harshidkhunt09@gmail.com>
2022-06-29 19:02:43 +0530

    Update ArticlePage

--- my-blog-frontend/src/pages/ArticlePage.js
+++ my-blog-frontend/src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import ArticleList from '../components/ArticleList';
@@ -8,38 +8,38 @@ import AddCommentForm from '../components/AddCommentForm';
 import NotFoundPage from './NotFoundPage';
 import useToken from '../auth/useToken';
 
-const ArticlePage = ({ articleList, articleInfo, setArticleInfo }) => {
+const ArticlePage = ({ articleList }) => {
   // eslint-disable-next-line no-unused-vars
   const [token, setToken] = useToken();
   const { name } = useParams();
   const navigate = useNavigate();
   const article = articleList.find((article) => article.name === name);
 
-  // const [articleInfo, setArticleInfo] = useState({
-  //   name: '',
-  //   upvotes: 0,
-  //   comments: [{ username: '', text: '' }],
-  // });
+  const [articleInfo, setArticleInfo] = useState({
+    name: '',
+    upvotes: 0,
+    comments: [{ username: '', text: '' }],
+  });
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const result = await fetch(`/api/articles/${name}`, {
-  //         method: 'post',
-  //         headers: {
-  //           Accept: 'application/json',
-  //           'Content-Type': 'application/json',
-  //         },
-  //         credentials: 'include',
-  //       });
-  //       const body = await result.json();
-  //       setArticleInfo(body);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
-  //   fetchData();
-  // }, [name]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await fetch(`/api/articles/${name}`, {
+          method: 'post',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+        });
+        const body = await result.json();
+        setArticleInfo(body);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [name]);
 
   useEffect(() => {
     const fetchData = async () => {
