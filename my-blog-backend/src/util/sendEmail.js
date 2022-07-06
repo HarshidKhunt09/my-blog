6a4f05b148b042b9518be1cbc768de5dee47617b
Harshid Khunt <harshidkhunt09@gmail.com>
2022-07-06 19:10:20 +0530

    send grid api

--- my-blog-backend/src/util/sendEmail.js
+++ my-blog-backend/src/util/sendEmail.js
@@ -1,8 +1,6 @@
 import sendgrid from '@sendgrid/mail';
 
-sendgrid.setApiKey(
-  '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'
-);
+sendgrid.setApiKey();
 
 const sendEmail = ({ to, from, subject, text, html }) => {
   const msg = { to, from, subject, text, html };
